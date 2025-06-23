6b8c63e0be0efab6056cd515332370c25ecd6294
nara <parknara0213@gmail.com>
2025-06-23 12:52:25 +0900

    0623 오전 정리

---
+++ study/0623/main.js
@@ -0,0 +1,19 @@
+// alert("Hello, world!!!");
+// console.log("Hello, world!!");
+// document.write("<h1>Document Write</h1>");
+
+// 변수 실습
+// let currentYear = 2025;
+// let birthYear = prompt("태어날 해를 입력하세요.");
+// let age = currentYear - birthYear + 1;
+
+// alert("당신의 나이는 " + age + "살입니다.");
+
+// 자료형 실습
+let name = "철수";
+let age = 20;
+let isStudent = true;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isStudent); // boolean
