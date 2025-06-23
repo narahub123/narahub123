b753671f4d1c7a19876789c68a7ad023a1e8a939
nara <parknara0213@gmail.com>
2025-06-23 15:17:43 +0900

    조건문 정리

--- study/0623/main.js
+++ study/0623/main.js
@@ -19,54 +19,77 @@
 // console.log(typeof isStudent); // boolean
 
 // 연산자 실습
-let price = 10000;
-let rate = 0.2;
-let discount = price * rate;
-let finalPrice = price - discount;
+// let price = 10000;
+// let rate = 0.2;
+// let discount = price * rate;
+// let finalPrice = price - discount;
 
-console.log("할인된 가격은 " + finalPrice + "원입니다.");
+// console.log("할인된 가격은 " + finalPrice + "원입니다.");
 
 // 템플릿 리터럴
-let name = "영희";
-console.log(`안녕하세요. ${name}님!`);
-console.log("안녕하세요. " + name + "님!");
+// let name = "영희";
+// console.log(`안녕하세요. ${name}님!`);
+// console.log("안녕하세요. " + name + "님!");
 
 // 실습 : 복합 연산자
-let a = 10;
-console.log(a);
+// let a = 10;
+// console.log(a);
 
-a += 5;
-console.log(a);
+// a += 5;
+// console.log(a);
 
-a -= 3;
-console.log(a);
+// a -= 3;
+// console.log(a);
 
-a *= 2;
-console.log(a);
+// a *= 2;
+// console.log(a);
 
-a /= 4;
-console.log(a);
+// a /= 4;
+// console.log(a);
 
-console.log(`최종 값은 ${a}입니다.`);
+// console.log(`최종 값은 ${a}입니다.`);
 
 // 1. 이름과 나이를 입력받아 인사말 출력하기 => confirm + alert
-let userName = prompt("이름을 입력헤주세요.");
-let userAge = prompt("나이를 입력헤주세요.");
+// let userName = prompt("이름을 입력헤주세요.");
+// let userAge = prompt("나이를 입력헤주세요.");
 
-alert(`안녕하세요. ${userName}님.\n당신의 나이는 ${userAge}입니다.`);
+// alert(`안녕하세요. ${userName}님.\n당신의 나이는 ${userAge}입니다.`);
 
 // 2. 두 수를 입력 받아 사칙 연산 결과를 각각 출력하시오
-let left = Number(prompt("숫자를 입력해주세요"));
-let right = Number(prompt("다른 숫자를 입력해주세요."));
+// let left = Number(prompt("숫자를 입력해주세요"));
+// let right = Number(prompt("다른 숫자를 입력해주세요."));
 
-alert(
-  `${left} + ${right} = ${left + right} \n${left} - ${right} = ${
-    left - right
-  } \n${left} * ${right} = ${left * right}\n${left} / ${right} = ${
-    left / right
-  }`
-);
+// alert(
+//   `${left} + ${right} = ${left + right} \n${left} - ${right} = ${
+//     left - right
+//   } \n${left} * ${right} = ${left * right}\n${left} / ${right} = ${
+//     left / right
+//   }`
+// );
 
 // 3. == vs === 비교 실험하기
-console.log(`1 == "1"은 ${1 == "1"}`);
-console.log(`1 === "1"은 ${1 === "1"}`);
+// console.log(`1 == "1"은 ${1 == "1"}`);
+// console.log(`1 === "1"은 ${1 === "1"}`);
+
+// if 문 실습 : 3의 배수 검사기
+// let num = prompt("숫자를 입력하세요.");
+
+// if (num % 3 === 0) {
+//   alert("3의 배수입니다.");
+// } else {
+//   alert("3의 배수가 아닙니다.");
+// }
+
+// switch 문 실습 :
+let day = prompt("요일을 입력하세요.");
+
+switch (day) {
+  case "월요일":
+    alert("한 주의 시작!");
+    break;
+  case "금요일":
+    alert("불타는 금요일!");
+    break;
+  default:
+    alert("평범한 하루네요.");
+}
