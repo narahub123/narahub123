d31bf8953405fc67a96c46cc73737a0343a6051f
nara <parknara0213@gmail.com>
2025-06-29 10:47:58 +0900

    masonry layout : masonry 적용

--- toys/masonry/masonry.js
+++ toys/masonry/masonry.js
@@ -10,6 +10,34 @@ const items = [
     height: 400,
     icon: '<i class="fi fi-rr-refresh icon"></i>',
   },
+  {
+    name: "test",
+    height: 500,
+  },
+  {
+    name: "test",
+    height: 200,
+  },
+  {
+    name: "test",
+    height: 200,
+  },
+  {
+    name: "test",
+    height: 500,
+  },
+  {
+    name: "test",
+    height: 400,
+  },
+  {
+    name: "test",
+    height: 300,
+  },
+  {
+    name: "test",
+    height: 200,
+  },
 ];
 
 const layout = document.querySelector("#layout");
@@ -17,19 +45,39 @@ console.log(layout);
 
 window.addEventListener("load", function () {
   for (let i = 0; i < items.length; i++) {
-    const item = createItem();
+    const item = createItem(i);
     console.log(item);
 
     layout.appendChild(item);
   }
+
+  masonryLayout(layout);
 });
 
-function createItem() {
+function createItem(num) {
   const item = document.createElement("div");
 
   item.setAttribute("class", "item");
 
   item.style.backgroundColor = "lightblue";
 
+  item.style.height = `${items[num].height}px`;
+
   return item;
+}
+
+function masonryLayout(layout) {
+  const layoutStyle = getComputedStyle(layout);
+
+  const rowHeight = parseInt(layoutStyle.getPropertyValue("grid-auto-rows"));
+  const rowGap = parseInt(layoutStyle.getPropertyValue("row-gap"));
+
+  const items = document.querySelectorAll(".item");
+
+  items.forEach((item) => {
+    const height = item.getBoundingClientRect().height;
+    const span = Math.ceil((height + rowGap) / (rowHeight + rowGap));
+
+    item.style.gridRowEnd = `span ${span}`;
+  });
 }
