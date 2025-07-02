1b2929a9133cef79b3185103a612d85015879323
nara <parknara0213@gmail.com>
2025-07-02 12:33:33 +0900

    never, intersection, union 실습

---
+++ study/0702/union.js
@@ -0,0 +1,6 @@
+var unionObj = {
+    //   name: "John",
+    name: "sue",
+    age: 25,
+};
+console.log(unionObj);
---
+++ study/0702/union.ts
@@ -0,0 +1,7 @@
+let unionObj: { name: string } | { age: number } = {
+  //   name: "John",
+  name: "sue",
+  age: 25,
+};
+
+console.log(unionObj);
