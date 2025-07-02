30cc3787be9ce2abeb8f65a3b017d0877a2d28b8
nara <parknara0213@gmail.com>
2025-07-02 11:53:23 +0900

    any, unknown 타입 연습

---
+++ study/0702/any.ts
@@ -0,0 +1,17 @@
+let value: any = 10;
+
+console.log(value);
+
+value = "hello"; // ok
+
+console.log(value);
+
+value = new Array();
+
+console.log(value);
+
+value.push(33);
+
+console.log(value);
+
+
---
+++ study/0702/unknown.js
@@ -0,0 +1,10 @@
+var value2 = 10;
+console.log(value2);
+value2 = "hello";
+console.log(value2);
+value2 = new Array();
+console.log(value2);
+if (value2 instanceof Array) {
+    value2.push(33);
+}
+console.log(value2);
