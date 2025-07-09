87fdeed486660550447ebdb889f46b43cca91437
nara <parknara0213@gmail.com>
2025-07-09 12:19:10 +0900

    0709: 이벤트 - eventListener 실습

--- study/0709/ch02_4/src/App.tsx
+++ study/0709/ch02_4/src/App.tsx
@@ -1,20 +1,12 @@
 import "./App.css";
+import EventListener from "./pages/EventListener";
 
-function App() {
-  // key
-  // const texts = [<p key={0}>Hello</p>, <p key={0}>world</p>];
-
-  // map
-  // const texts = ["hello", "world"].map((text, index) => (
-  //   <p key={index}>{text}</p>
-  // ));
-
-  // children
-  const texts = ["hello", "world"].map((text, index) => (
-    <p key={index} children={text} />
-  ));
-
-  return <div children={texts} />;
-}
+const App = () => {
+  return (
+    <div>
+      <EventListener />
+    </div>
+  );
+};
 
 export default App;
