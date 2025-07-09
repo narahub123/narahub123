f06a9e454153ed8247d18470d023da380fb99532
nara <parknara0213@gmail.com>
2025-07-09 16:21:05 +0900

    0709 css - 컴포넌트 생성

--- study/0709/ch03_1/src/App.tsx
+++ study/0709/ch03_1/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Style from "./pages/Style";
+import UsingIcon from "./pages/UsingIcon";
 
 const App = () => {
   return (
     <div>
-      <Style />
+      <UsingIcon />
     </div>
   );
 };
--- study/0709/ch03_1/src/pages/UsingIcon.tsx
+++ study/0709/ch03_1/src/pages/UsingIcon.tsx
@@ -1,5 +1,12 @@
-const CopyMe = () => {
-  return <div>CopyMe</div>;
+import Icon from "../components/Icon";
+
+const UsingIcon = () => {
+  return (
+    <div>
+      <h1>UsingIcon</h1>
+      <Icon name="home" style={{ color: "blue", fontSize: "30" }} />
+    </div>
+  );
 };
 
-export default CopyMe;
+export default UsingIcon;
