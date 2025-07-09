639ad827512ba5be9e57a83a24e55329781cef8c
nara <parknara0213@gmail.com>
2025-07-09 14:31:21 +0900

    0709 : 이벤트 - 드래그 앤 드롭

--- study/0709/ch02_4/src/App.tsx
+++ study/0709/ch02_4/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import DragDrop from "./pages/DragDrop";
 import EventBubbling from "./pages/EventBubbling";
 import EventListener from "./pages/EventListener";
 import FileInput from "./pages/FileDrop";
@@ -7,10 +8,11 @@ import ReactOnClick from "./pages/ReactOnClick";
 const App = () => {
   return (
     <div>
-      <EventListener />
+      {/* <EventListener />
       <ReactOnClick />
       <EventBubbling />
-      <FileInput />
+      <FileInput /> */}
+      <DragDrop />
     </div>
   );
 };
--- study/0709/ch02_4/src/pages/DragDrop.tsx
+++ study/0709/ch02_4/src/pages/DragDrop.tsx
@@ -1,5 +1,29 @@
-const CopyMe = () => {
-  return <div>CopyMe</div>;
+import { DragEvent } from "react";
+
+const DragDrop = () => {
+  const onDragStart = (e: DragEvent<HTMLElement>) => {
+    console.log("onDragStart", e.dataTransfer);
+  };
+  const onDragEnter = (e: DragEvent<HTMLElement>) => {
+    console.log("onDragEnter", e.dataTransfer);
+  };
+  const onDragOver = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault();
+  };
+  const onDrop = (e: DragEvent<HTMLElement>) => {
+    console.log("onDrop", e.dataTransfer);
+  };
+  return (
+    <div>
+      <p>DragDrop</p>
+      <div draggable onDragStart={onDragStart} onDragEnter={onDragEnter}>
+        <h1>Drag Me</h1>
+      </div>
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drop Over Me</h1>
+      </div>
+    </div>
+  );
 };
 
-export default CopyMe;
+export default DragDrop;
