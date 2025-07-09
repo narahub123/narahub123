546dcd3ee6197109785d4fd709134bff954b7852
nara <parknara0213@gmail.com>
2025-07-09 15:31:07 +0900

    0709: css - Bootstrap 실습

---
+++ study/0709/ch03_1/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Bootstrap from "./pages/Bootstrap";
+
+const App = () => {
+  return (
+    <div>
+      <Bootstrap />
+    </div>
+  );
+};
+
+export default App;
---
+++ study/0709/ch03_1/src/pages/UsingIcon.tsx
@@ -0,0 +1,5 @@
+const CopyMe = () => {
+  return <div>CopyMe</div>;
+};
+
+export default CopyMe;
