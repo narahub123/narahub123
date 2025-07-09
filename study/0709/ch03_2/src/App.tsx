7e749b76113719de165a7291464c9d0a3eb5665a
nara <parknara0213@gmail.com>
2025-07-09 16:40:36 +0900

    0709 css tailwind 설치하기

---
+++ study/0709/ch03_2/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Style from "./pages/Style";
+import UsingIcon from "./pages/UsingIcon";
+
+const App = () => {
+  return (
+    <div>
+      <UsingIcon />
+    </div>
+  );
+};
+
+export default App;
