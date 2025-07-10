0c55923e6371a7b6caf2b6dcfb7fa9d5715ca4d5
nara <parknara0213@gmail.com>
2025-07-10 21:11:25 +0900

    테일윈즈 설치

--- study/0710/ch03_2/src/pages/TextsTest.tsx
+++ study/0710/ch03_2/src/pages/TextsTest.tsx
@@ -1,25 +1,5 @@
-import { Paragraph, SubTitle, Summary, Title } from "../components";
-import * as D from "../data";
-
-const paragraphs = D.makeArray(2).map(D.randomParagraphs).join("\n");
-const summary = D.makeArray(3).map(D.randomSentence).join("\n");
-
-const TextsTest = () => {
-  return (
-    <div>
-      <Title>TextsTest</Title>
-      <div>
-        <Title className="text-blue-600">{D.randomTitleText()}</Title>
-        <SubTitle className="text-blue-400">{D.randomSentence()}</SubTitle>
-        <p className="text-xl italic text-center text-gray-900 text-bold">
-          {D.randomName()}
-        </p>
-        <Paragraph numberOfLines={5}>{paragraphs}</Paragraph>
-        <Summary className="text-center text-gray-500">{summary}</Summary>
-        <p className="text-center text-pink-400">{D.randomRelativeDate()}</p>
-      </div>
-    </div>
-  );
+const CopyMe = () => {
+  return <div>CopyMe</div>;
 };
 
-export default TextsTest;
+export default CopyMe;
