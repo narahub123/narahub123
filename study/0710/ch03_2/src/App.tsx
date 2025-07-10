1d3159a83c99705a39caeca926434ed115382128
nara <parknara0213@gmail.com>
2025-07-10 14:21:52 +0900

    tailwindcss 실습 - 적용이 안되는 문제 발생

---
+++ study/0710/ch03_2/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+// import Tailwindcss from "./pages/Tailwindcss";
+import TextsTest from "./pages/TextsTest";
+// import Color from "./pages/Color";
+// import TextsTest from "./pages/TextsTest";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Tailwindcss />
+      <Color /> */}
+      <TextsTest />
+    </div>
+  );
+};
+
+export default App;
