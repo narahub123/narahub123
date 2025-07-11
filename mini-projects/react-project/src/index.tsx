d92dcfa63d900cbc9a142c5cb2423e99ba369b9d
nara <parknara0213@gmail.com>
2025-07-11 14:59:40 +0900

    feat: React-Project Masonry 컴포넌트 생성

--- mini-projects/react-project/src/index.tsx
+++ mini-projects/react-project/src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import "@fontsource/material-icons";
+import { Masonry } from "./layouts";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Masonry></Masonry>
   </React.StrictMode>
 );
---
+++ mini-projects/react-project/src/layouts/Masonry.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+type MasonryProps = {};
+
+export const Masonry: FC<MasonryProps> = ({}) => {
+  return <></>;
+};
