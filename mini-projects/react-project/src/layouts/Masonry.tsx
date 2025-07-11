3e3d755293df08aa74f8b263d2b7495fd69e7c34
nara <parknara0213@gmail.com>
2025-07-11 15:41:01 +0900

    feat: Masonry - grid와 grid-auto-rows를 이용한 Masonry 레이아웃 구현

--- mini-projects/react-project/src/layouts/Masonry.tsx
+++ mini-projects/react-project/src/layouts/Masonry.tsx
@@ -1,7 +1,13 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
-type MasonryProps = {};
+type MasonryProps = {
+  children: ReactNode;
+};
 
-export const Masonry: FC<MasonryProps> = ({}) => {
-  return <></>;
+export const Masonry: FC<MasonryProps> = ({ children }) => {
+  return (
+    <div className="grid w-full h-screen gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-[1rem]">
+      {children}
+    </div>
+  );
 };
