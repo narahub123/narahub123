a76d67e9ce37c4e376fec281e548ead04bbc3ed5
nara <parknara0213@gmail.com>
2025-07-13 01:55:26 +0900

    feat: MemoryGame FlipCard 생성

---
+++ mini-projects/react-project/src/features/MemoryGame/components/FlipCard.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import { IFlipCard } from "../types";
+import { useCardFontSize } from "../hooks";
+
+type FlipCardProps = {
+  card: IFlipCard;
+  level: number;
+  onClick: (card: IFlipCard) => void;
+};
+
+export const FlipCard: FC<FlipCardProps> = ({ card, onClick, level }) => {
+  const { index, icon, isFlipped } = card;
+
+  const fontSize = useCardFontSize(level * 2);
+
+  return (
+    <button
+      className={`relative w-full aspect-square rounded-lg  transition-transform duration-500`}
+      style={{
+        transformStyle: "preserve-3d",
+        transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)",
+      }}
+      onClick={() => onClick(card)}
+    >
+      {/* 앞면 */}
+      <div
+        className="absolute inset-0 flex items-center justify-center w-full h-full bg-white border border-gray-300"
+        style={{
+          backfaceVisibility: "hidden",
+          fontSize,
+        }}
+      >
+        {index + 1}
+      </div>
+
+      {/* 뒷면 */}
+      <div
+        className="absolute inset-0 flex items-center justify-center w-full h-full bg-blue-300"
+        style={{
+          backfaceVisibility: "hidden",
+          transform: "rotateY(180deg)",
+          fontSize,
+        }}
+      >
+        {icon}
+      </div>
+    </button>
+  );
+};
--- mini-projects/react-project/src/features/MemoryGame/components/GameBoard.tsx
+++ mini-projects/react-project/src/features/MemoryGame/components/GameBoard.tsx
@@ -1,11 +1,41 @@
 import { FC } from "react";
+import { IFlipCard } from "../types";
+import { FlipCard } from "./FlipCard";
 
 type GameBoardProps = {
   isGameOn: boolean;
+  level: number | "";
+  cards: IFlipCard[];
+  handleFlipCard: (card: IFlipCard) => void;
 };
 
-export const GameBoard: FC<GameBoardProps> = ({ isGameOn }) => {
-  if (!isGameOn) return null;
+export const GameBoard: FC<GameBoardProps> = ({
+  isGameOn,
+  level,
+  cards,
+  handleFlipCard,
+}) => {
+  if (!isGameOn || !level) return null;
 
-  return <main className="w-full">게임판</main>;
+  const gridCols = {
+    1: "grid-cols-2",
+    2: "grid-cols-4",
+    3: "grid-cols-6",
+    4: "grid-cols-8",
+  }[level];
+
+  return (
+    <main className={`grid w-full gap-4 ${gridCols} mt-4 p-4`}>
+      {cards.map((card) => {
+        return (
+          <FlipCard
+            card={card}
+            key={card.index}
+            onClick={handleFlipCard}
+            level={level}
+          />
+        );
+      })}
+    </main>
+  );
 };
