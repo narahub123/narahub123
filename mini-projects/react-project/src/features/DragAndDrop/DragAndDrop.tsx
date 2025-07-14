9433089013e97f33ca32aa5334a1f225020408eb
nara <parknara0213@gmail.com>
2025-07-14 20:26:41 +0900

    feat: DragAndDrop 파일 유효성 검사 추가

--- mini-projects/react-project/src/features/DragAndDrop/DragAndDrop.tsx
+++ mini-projects/react-project/src/features/DragAndDrop/DragAndDrop.tsx
@@ -3,9 +3,12 @@ import { DropBox } from "./components";
 
 export const DragAndDrop = () => {
   const [images, setImages] = useState<File[]>([]);
+
+  console.log(images);
+
   return (
     <div className="w-[50%]">
-      <DropBox setImages={setImages} />
+      <DropBox setImages={setImages} images={images} />
       <div className="w-full">이미지 프리뷰</div>
     </div>
   );
