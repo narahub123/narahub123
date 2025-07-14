796d6d3be991e19a7befda342f3021cfe14a7aba
nara <parknara0213@gmail.com>
2025-07-14 15:12:44 +0900

    ch03_5 준비

---
+++ study/0714/ch03_5/src/components/Icon.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+export type IconProps = {
+  name: string;
+  style?: React.CSSProperties;
+  className?: string;
+};
+
+export const Icon: FC<IconProps> = ({ name, style, className }) => {
+  return (
+    <span className={`"material-icons" ${className}`} style={style}>
+      {name}
+    </span>
+  );
+};
