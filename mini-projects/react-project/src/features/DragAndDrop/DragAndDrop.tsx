a3ceba2cdf5fcb1f2d8df728f53729fdc10d95d1
nara <parknara0213@gmail.com>
2025-07-14 15:01:52 +0900

    feat : DragAndDrop DropBox 드래그 이벤트 추가

--- mini-projects/react-project/src/features/DragAndDrop/DragAndDrop.tsx
+++ mini-projects/react-project/src/features/DragAndDrop/DragAndDrop.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { DropBox } from "./components";
 
 export const DragAndDrop = () => {
+  const [images, setImages] = useState<(File | FileList)[]>([]);
   return (
     <div className="w-[50%]">
-      <DropBox />
+      <DropBox setImages={setImages} />
       <div className="w-full">이미지 프리뷰</div>
     </div>
   );
