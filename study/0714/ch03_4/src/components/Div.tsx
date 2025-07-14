8971ec49c0acce21755cde858e43178356583ed9
nara <parknara0213@gmail.com>
2025-07-14 12:16:15 +0900

    MinMaxWidthHeight, LeftRightTopBottom 타입 추가

--- study/0714/ch03_4/src/components/Div.tsx
+++ study/0714/ch03_4/src/components/Div.tsx
@@ -1,19 +1,45 @@
 import { DetailedHTMLProps, FC, HTMLAttributes } from "react";
 import { WidthHeight } from "./WidthHeight";
+import { MinMaxWidthHeight } from "./MinMaxWidthHeight";
+import { LeftRightTopBottom } from "./LeftRightTopBottom";
 
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >;
 
-export type DivProps = ReactDivProps & WidthHeight;
+export type DivProps = ReactDivProps &
+  WidthHeight &
+  LeftRightTopBottom &
+  MinMaxWidthHeight;
 
 export const Div: FC<DivProps> = ({
   width,
   height,
+  left,
+  right,
+  top,
+  bottom,
+  minWidth,
+  maxWidth,
+  minHeight,
+  maxHeight,
   style: _style,
   ...props
 }) => {
-  const style = { ..._style, width, height };
+  const style = {
+    ..._style,
+    width,
+    height,
+    left,
+    right,
+    top,
+    bottom,
+    minWidth,
+    maxWidth,
+    minHeight,
+    maxHeight,
+  };
+
   return <div {...props} style={style} />;
 };
