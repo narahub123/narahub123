b18860c3aea3ba1d665c83da6508fc9316f2452e
nara <parknara0213@gmail.com>
2025-07-14 15:12:44 +0900

    ch03_5 준비

---
+++ study/0714/ch03_5/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import CardContainer from "./pages/CardContainer";
+import DirectionTest from "./pages/DirectionTest";
+import JustifyCenterTest from "./pages/JustifyCenterTest";
+import UserContainer from "./pages/UserContainer";
+
+function App() {
+  return (
+    <main>
+      {/* <DirectionTest /> */}
+      {/* <JustifyCenterTest /> */}
+      <UserContainer />
+      <CardContainer />
+    </main>
+  );
+}
+
+export default App;
