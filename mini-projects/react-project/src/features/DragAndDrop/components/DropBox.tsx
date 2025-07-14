125e84584746653698af6ccc517062ed00e96fab
nara <parknara0213@gmail.com>
2025-07-14 18:15:13 +0900

    feat : DropBox input:file을 이용한 이미지 추가

--- mini-projects/react-project/src/features/DragAndDrop/components/DropBox.tsx
+++ mini-projects/react-project/src/features/DragAndDrop/components/DropBox.tsx
@@ -1,10 +1,11 @@
-import { FC, useState } from "react";
+import { FC, useRef, useState } from "react";
 
 export type DropBoxProps = {
   setImages: React.Dispatch<React.SetStateAction<File[]>>;
 };
 
 export const DropBox: FC<DropBoxProps> = ({ setImages }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [isIn, setIsIn] = useState(false);
 
   const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
@@ -34,15 +35,39 @@ export const DropBox: FC<DropBoxProps> = ({ setImages }) => {
     setIsIn((prev) => (prev !== false ? false : prev));
   };
 
+  const handleClick = () => {
+    if (!inputRef.current) return;
+
+    inputRef.current.click();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files ?? []);
+
+    if (files.length === 0) return;
+
+    setImages(files);
+  };
+
   return (
     <div
-      className="flex items-center justify-center w-full border-2 border-blue-400 border-dashed aspect-square"
+      className="flex items-center justify-center w-full border-2 border-blue-400 border-dashed cursor-pointer aspect-square rounded-2xl"
       onDragEnter={handleDragEnter}
       onDragLeave={handleDragLeave}
       onDragOver={handleDragOver}
       onDrop={handleDrop}
+      onClick={handleClick}
     >
-      <p>여기에 드래그 해라</p>
+      <p>사진을 드래그하세요.</p>
+      <input
+        type="file"
+        className="hidden"
+        hidden
+        multiple
+        accept="images/*"
+        onChange={handleChange}
+        ref={inputRef}
+      />
     </div>
   );
 };
