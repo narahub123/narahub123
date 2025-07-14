a25a7eabc9f8cfaf67b4ea9891922695240a261c
nara <parknara0213@gmail.com>
2025-07-14 14:48:52 +0900

    Card 컴포넌트 구현하기

--- study/0714/ch03_4/src/components/Icon.tsx
+++ study/0714/ch03_4/src/components/Icon.tsx
@@ -3,14 +3,13 @@ import { FC } from "react";
 export type IconProps = {
   name: string;
   style?: React.CSSProperties;
+  className?: string;
 };
 
-const Icon: FC<IconProps> = ({ name, style }) => {
+export const Icon: FC<IconProps> = ({ name, style, className }) => {
   return (
-    <span className="material-icons" style={style}>
+    <span className={`"material-icons" ${className}`} style={style}>
       {name}
     </span>
   );
 };
-
-export default Icon;
