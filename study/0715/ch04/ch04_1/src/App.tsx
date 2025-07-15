1aba142135bc4b15554fc06654d58b90aed221aa
nara <parknara0213@gmail.com>
2025-07-15 15:37:05 +0900

    0715 시계 만들기 - useState, useEffect 사용

--- study/0715/ch04/ch04_1/src/App.tsx
+++ study/0715/ch04/ch04_1/src/App.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Clock from "./pages/Clock";
 
 function App() {
-  const today = new Date();
+  const [today, setToday] = useState(new Date());
+
+  useEffect(() => {
+    console.log("useEffect called");
+
+    const duration = 1000;
+
+    const id = setInterval(() => {
+      setToday(new Date());
+      console.log("today", today.toLocaleTimeString());
+    }, duration);
+
+    return () => clearInterval(id);
+  }, []);
+
   return (
     <main>
       <Clock today={today} />
