8b2db4d8913104e3c55ec4d778f16b9647603711
nara <parknara0213@gmail.com>
2025-07-15 16:12:58 +0900

    0715 useState 연습 문제

--- study/0715/ch04/ch04_1/src/App.tsx
+++ study/0715/ch04/ch04_1/src/App.tsx
@@ -1,12 +1,32 @@
-import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import Clock from "./pages/Clock";
-import { useClock } from "./hooks";
+import { useState } from "react";
 
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
   return (
     <main>
-      <Clock today={useClock()} />
+      <div>
+        <h2>{"뎃셈기"}</h2>
+        <div className="flex flex-row items-center">
+          <input
+            type="number"
+            value={num1}
+            onChange={(e) => setNum1(parseInt(e.target.value))}
+            className="w-[100px] text-center"
+          />
+          <span>{"+"}</span>
+          <input
+            type="number"
+            value={num2}
+            onChange={(e) => setNum2(parseInt(e.target.value))}
+            className="w-[100px] text-center"
+          />
+        </div>
+
+        <p>{`덧셈 결과는 : ${num1} + ${num2} = ${num1 + num2}`}</p>
+      </div>
     </main>
   );
 }
