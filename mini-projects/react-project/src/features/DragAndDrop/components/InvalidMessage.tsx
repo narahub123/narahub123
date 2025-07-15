44492f6ea39e437cdef0c01aff6ba7c47c634e13
nara <parknara0213@gmail.com>
2025-07-15 11:37:16 +0900

    feat : DropAndDrop InvalidMessage 생성

---
+++ mini-projects/react-project/src/features/DragAndDrop/components/InvalidMessage.tsx
@@ -0,0 +1,31 @@
+import { FC, useEffect } from "react";
+
+export type InvalidMessageProps = {
+  message: string;
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const InvalidMessage: FC<InvalidMessageProps> = ({
+  message,
+  setMessage,
+}) => {
+  useEffect(() => {
+    if (!message) return;
+
+    const timer = setTimeout(() => {
+      setMessage("");
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message]);
+
+  if (!message) return null;
+
+  return (
+    <div className="absolute flex items-center justify-center w-[80%] p-4 text-white -translate-x-1/2 -translate-y-1/2 bg-red-400 rounded-lg top-1/2 left-1/2">
+      {message}
+    </div>
+  );
+};
