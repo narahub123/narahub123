dba73157565465174d2616ce8df18c600120933c
nara <parknara0213@gmail.com>
2025-07-15 15:08:51 +0900

    0715  Clock 컴포넌트 생성

---
+++ study/0715/ch04/ch04_1/src/theme/daisyui/Button.tsx
@@ -0,0 +1,23 @@
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  FC,
+  PropsWithChildren,
+} from "react";
+
+export type ReactButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  PropsWithChildren;
+
+export type ButtonProps = ReactButtonProps & {};
+
+export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+  className: _className,
+  ...buttonProps
+}) => {
+  const className = ["btn", _className].join(" ");
+  
+  return <button {...buttonProps} className={className} />;
+};
