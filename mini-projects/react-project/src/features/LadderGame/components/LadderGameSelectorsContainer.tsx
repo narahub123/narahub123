bc891516aba50ba3314f0544bf5c357b13a33521
nara <parknara0213@gmail.com>
2025-07-16 00:50:14 +0900

    feat: LadderGame LadderGameSelectorsContainer 생성

---
+++ mini-projects/react-project/src/features/LadderGame/components/LadderGameSelectorsContainer.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export type LadderGameSelectorsContainerProps = {};
+
+export const LadderGameSelectorsContainer: FC<
+  LadderGameSelectorsContainerProps
+> = ({}) => {
+  return <div className="flex flex-row w-full justify-evenly"></div>;
+};
