224d336bd2e018400fc62963e9a0e096a77b9f71
nara <parknara0213@gmail.com>
2025-07-15 16:01:03 +0900

    0715 커스텀 훅 생성

--- study/0715/ch04/ch04_1/src/App.tsx
+++ study/0715/ch04/ch04_1/src/App.tsx
@@ -1,26 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Clock from "./pages/Clock";
+import { useClock } from "./hooks";
 
 function App() {
-  const [today, setToday] = useState(new Date());
-
-  useEffect(() => {
-    console.log("useEffect called");
-
-    const duration = 1000;
-
-    const id = setInterval(() => {
-      setToday(new Date());
-      console.log("today", today.toLocaleTimeString());
-    }, duration);
-
-    return () => clearInterval(id);
-  }, []);
-
   return (
     <main>
-      <Clock today={today} />
+      <Clock today={useClock()} />
     </main>
   );
 }
