befebf96b1cdb279be37d9aad579cfa872cef5d3
nara <parknara0213@gmail.com>
2025-07-16 20:30:39 +0900

    feat: LadderGame LadderGameLadderContainer 기본 선 그리기

--- mini-projects/react-project/src/features/LadderGame/components/LadderGameGround.tsx
+++ mini-projects/react-project/src/features/LadderGame/components/LadderGameGround.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import {
   LadderGameLadderContainer,
   LadderGameResultsContainer,
   LadderGameSelectorsContainer,
 } from "../components";
+import { SelectorPosition } from "../types";
 
 export type LadderGameGroundProps = {
   isStarted: boolean;
@@ -14,15 +15,22 @@ export const LadderGameGround: FC<LadderGameGroundProps> = ({
   isStarted,
   participants,
 }) => {
+  const [selectorPositions, setSelectorPositions] = useState<
+    SelectorPosition[]
+  >([]);
+
   return (
     <div className="flex flex-col w-full gap-4 mt-4">
       <LadderGameSelectorsContainer
         isStarted={isStarted}
         participants={participants}
+        selectorPositions={selectorPositions}
+        setSelectorPositions={setSelectorPositions}
       />
       <LadderGameLadderContainer
         isStarted={isStarted}
         participants={participants}
+        selectorPositions={selectorPositions}
       />
       <LadderGameResultsContainer
         isStarted={isStarted}
