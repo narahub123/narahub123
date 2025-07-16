bd19baf2f25bb5c91b76cb821b6ceb7d2e7d30bc
nara <parknara0213@gmail.com>
2025-07-16 13:37:51 +0900

    feat: LadderGameSelector ref 삭제

--- mini-projects/react-project/src/features/LadderGame/components/LadderGameSelector.tsx
+++ mini-projects/react-project/src/features/LadderGame/components/LadderGameSelector.tsx
@@ -9,33 +9,6 @@ export const LadderGameSelector: FC<LadderGameSelectorProps> = ({
   index,
   isStarted,
 }) => {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    if (!buttonRef.current) return;
-
-    const getButtonCenterPosition = () => {
-      if (!buttonRef.current) return;
-
-      // 버튼의 위치 가져오기
-      const { top, left, width, height } =
-        buttonRef.current.getBoundingClientRect();
-
-      const centerX = left + width / 2;
-      const centerY = top + height / 2;
-
-      console.log("버튼 중앙 위치", centerX, centerY);
-    };
-
-    // 초기 값
-    getButtonCenterPosition();
-
-    // resize 시 
-    window.addEventListener("resize", getButtonCenterPosition);
-
-    return () => window.removeEventListener("resize", getButtonCenterPosition);
-  }, []);
-
   return (
     <div className="relative w-10 aspect-square" key={`participant-${index}`}>
       <button
@@ -43,7 +16,6 @@ export const LadderGameSelector: FC<LadderGameSelectorProps> = ({
           isStarted ? "hover:scale-110" : ""
         }`}
         disabled={!isStarted} // 시작전에는 클릭 금지
-        ref={buttonRef}
       >
         {index + 1}
       </button>
