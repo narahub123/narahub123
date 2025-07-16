0841e420ae094a79f325374fd4a60d2951a3451b
nara <parknara0213@gmail.com>
2025-07-16 10:48:38 +0900

    feat: LadderGame LadderGameSelector 버튼의 가운데 위치 알아내기

--- mini-projects/react-project/src/features/LadderGame/components/LadderGameSelector.tsx
+++ mini-projects/react-project/src/features/LadderGame/components/LadderGameSelector.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 
 export type LadderGameSelectorProps = {
   index: number;
@@ -9,6 +9,33 @@ export const LadderGameSelector: FC<LadderGameSelectorProps> = ({
   index,
   isStarted,
 }) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (!buttonRef.current) return;
+
+    const getButtonCenterPosition = () => {
+      if (!buttonRef.current) return;
+
+      // 버튼의 위치 가져오기
+      const { top, left, width, height } =
+        buttonRef.current.getBoundingClientRect();
+
+      const centerX = left + width / 2;
+      const centerY = top + height / 2;
+
+      console.log("버튼 중앙 위치", centerX, centerY);
+    };
+
+    // 초기 값
+    getButtonCenterPosition();
+
+    // resize 시 
+    window.addEventListener("resize", getButtonCenterPosition);
+
+    return () => window.removeEventListener("resize", getButtonCenterPosition);
+  }, []);
+
   return (
     <div className="relative w-10 aspect-square" key={`participant-${index}`}>
       <button
@@ -16,6 +43,7 @@ export const LadderGameSelector: FC<LadderGameSelectorProps> = ({
           isStarted ? "hover:scale-110" : ""
         }`}
         disabled={!isStarted} // 시작전에는 클릭 금지
+        ref={buttonRef}
       >
         {index + 1}
       </button>
