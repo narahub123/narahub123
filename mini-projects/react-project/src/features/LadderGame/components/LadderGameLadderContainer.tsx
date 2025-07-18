e17dd496940012151c386be3b54a7e6c1373f02c
nara <parknara0213@gmail.com>
2025-07-18 22:28:20 +0900

    feat: LadderGame LadderGamePaths 생성

--- mini-projects/react-project/src/features/LadderGame/components/LadderGameLadderContainer.tsx
+++ mini-projects/react-project/src/features/LadderGame/components/LadderGameLadderContainer.tsx
@@ -1,24 +1,15 @@
 import { FC, useEffect, useLayoutEffect, useRef } from "react";
 import { useLadderGameContext } from "../hooks";
-import { LadderGameLadderMask, LadderGameLadder } from "../components";
-
-export type LadderGameLadderContainerProps = {};
+import {
+  LadderGameLadderMask,
+  LadderGameLadder,
+  LadderGamePaths,
+} from "../components";
 
 export const LadderGameLadderContainer: FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const pathRef = useRef<HTMLCanvasElement>(null);
 
-  const {
-    participants,
-    selectorPositions,
-    bridges,
-    paths,
-    rect,
-    setRect,
-    setCanvas,
-    setPathCanvas,
-  } = useLadderGameContext();
+  const { setRect } = useLadderGameContext();
 
   // 컴포넌트 크기 측정
   useLayoutEffect(() => {
@@ -29,58 +20,12 @@ export const LadderGameLadderContainer: FC = () => {
     setRect({ width, height });
   }, []);
 
-  // 경로 그리기
-  useEffect(() => {
-    if (paths.length === 0) return;
-    console.log(paths);
-    if (!pathRef.current) return;
-    const canvas = pathRef.current;
-    const ctx = pathRef.current?.getContext("2d")!;
-
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-
-    canvas.width = width;
-    canvas.height = height;
-
-    ctx.clearRect(0, 0, width, height);
-
-    function drawPath() {
-      ctx.lineWidth = 3;
-      ctx.strokeStyle = "red";
-
-      ctx.beginPath();
-
-      for (let i = 0; i < paths.length; i++) {
-        const path = paths[i];
-
-        if (i === 0) {
-          ctx.moveTo(path.centerX, path.centerY);
-        } else {
-          ctx.lineTo(path.centerX, path.centerY);
-        }
-      }
-
-      ctx.stroke();
-    }
-
-    drawPath();
-    setPathCanvas(canvas);
-  }, [paths]);
-
   return (
     <div className={`w-full flex-1 relative`} ref={containerRef}>
       {/* 컴포넌트로 빼기 */}
       <LadderGameLadderMask />
       <LadderGameLadder />
-      <canvas
-        className={`absolute top-0 left-0  flex flex-row w-full h-full justify-evenly`}
-        style={{
-          width: `${rect?.width}`,
-          height: `${rect?.height}`,
-        }}
-        ref={pathRef}
-      ></canvas>
+      <LadderGamePaths />
     </div>
   );
 };
