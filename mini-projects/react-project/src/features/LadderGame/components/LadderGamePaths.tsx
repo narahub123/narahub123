b2a558792a39ff33918ba998b0b803f415788c9c
nara <parknara0213@gmail.com>
2025-07-20 00:49:56 +0900

    feat: LadderGame requestAnimationFrame을 이용한 경로 이동 애니메이션 구현

--- mini-projects/react-project/src/features/LadderGame/components/LadderGamePaths.tsx
+++ mini-projects/react-project/src/features/LadderGame/components/LadderGamePaths.tsx
@@ -19,15 +19,19 @@ export const LadderGamePaths = () => {
     canvas.width = width;
     canvas.height = height;
 
-    ctx.clearRect(0, 0, width, height);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "red";
+
+    let curIndex = 0;
+    let progress = 0;
+    let speed = 5; // 숫자가 클수록 빠름
 
     function drawPath() {
-      ctx.lineWidth = 3;
-      ctx.strokeStyle = "red";
-
+      ctx.clearRect(0, 0, width, height);
       ctx.beginPath();
 
-      for (let i = 0; i < paths.length; i++) {
+      // 차례대로 그리기 위해서는 paths의 길이가 아닌 curIndex 사용
+      for (let i = 0; i < curIndex; i++) {
         const path = paths[i];
 
         if (i === 0) {
@@ -37,7 +41,32 @@ export const LadderGamePaths = () => {
         }
       }
 
+      // curIndex가 paths.length보다 작은 경우
+      if (curIndex < paths.length) {
+        const start = paths[curIndex - 1] || paths[0];
+        const end = paths[curIndex];
+
+        if (end) {
+          const x = start.centerX + (end.centerX - start.centerX) * progress;
+          const y = start.centerY + (end.centerY - start.centerY) * progress;
+
+          if (curIndex === 0) ctx.moveTo(start.centerX, start.centerY);
+          ctx.lineTo(x, y);
+
+          progress += 0.02 * speed;
+
+          if (progress >= 1) {
+            progress = 0;
+            curIndex++;
+          }
+        }
+      }
+
       ctx.stroke();
+
+      if (curIndex < paths.length) {
+        requestAnimationFrame(drawPath);
+      }
     }
 
     drawPath();
