c37f45a33c4cc8bde17832602c821c5cfdb24787
nara <parknara0213@gmail.com>
2025-07-21 00:07:40 +0900

    feat: Dashboard 컴포넌트 열기 과정 수정

---
+++ mini-projects/react-project/src/hooks/useInjectComponent.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { useDashboardContext } from "./useDashboardContext";
+
+export const useInjectComponent = () => {
+  const { isCentered, setInnerComponent, cloneCard } = useDashboardContext();
+  useEffect(() => {
+    if (!isCentered || !cloneCard) return;
+
+    if (cloneCard.component) setInnerComponent(cloneCard.component);
+  }, [isCentered]);
+};
