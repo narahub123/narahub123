4cec1f8de3e67cac0375bc128dc82934a82078e5
john <test@google.com>
2025-07-22 11:49:28 +0900

    0722 ch04_4 실습 준비

---
+++ study/0722/ch04_4/src/components/Div.tsx
@@ -0,0 +1,44 @@
+import { DetailedHTMLProps, FC, HTMLAttributes } from "react";
+import { WidthHeight } from "./WidthHeight";
+import { MinMaxWidthHeight } from "./MinMaxWidthHeight";
+import { LeftRightTopBottom } from "./LeftRightTopBottom";
+
+export type ReactDivProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+
+export type DivProps = ReactDivProps &
+  WidthHeight & { src?: string } & LeftRightTopBottom &
+  MinMaxWidthHeight;
+
+export const Div: FC<DivProps> = ({
+  width,
+  height,
+  left,
+  right,
+  top,
+  bottom,
+  minWidth,
+  maxWidth,
+  minHeight,
+  maxHeight,
+  style: _style,
+  ...props
+}) => {
+  const style = {
+    ..._style,
+    width,
+    height,
+    left,
+    right,
+    top,
+    bottom,
+    minWidth,
+    maxWidth,
+    minHeight,
+    maxHeight,
+  };
+
+  return <div {...props} style={style} />;
+};
