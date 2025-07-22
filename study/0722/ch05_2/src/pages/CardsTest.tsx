09f3e5f5633d19c653babe16f260d8711220e06d
narahub123 <parknara0213@gmail.com>
2025-07-22 15:29:49 +0900

    0722 combineReducers 사용을 위한 설정

---
+++ study/0722/ch05_2/src/pages/CardsTest.tsx
@@ -0,0 +1,8 @@
+export const CardsTest = () => {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">CardsTest</h2>
+      <div className="mt-4"></div>
+    </section>
+  );
+};
