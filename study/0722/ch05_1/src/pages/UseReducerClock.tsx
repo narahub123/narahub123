f026bd545cd3203cef8b173d4793a110234f7c8d
john <test@google.com>
2025-07-22 15:01:17 +0900

    0722 useReducer clock 생성

--- study/0722/ch05_1/src/pages/UseReducerClock.tsx
+++ study/0722/ch05_1/src/pages/UseReducerClock.tsx
@@ -1,8 +1,35 @@
+import { useReducer } from "react";
+import { AppState, SetTodayAction } from "../store";
+import { useInterval } from "../hooks";
+import { Div, Subtitle, Title } from "../components";
+
 export const UseReducerClock = () => {
+  const [{ today }, dispatch] = useReducer(
+    (state: AppState, action: SetTodayAction) => {
+      switch (action.type) {
+        case "setToday":
+          return { ...state, today: new Date() };
+      }
+
+      return state;
+    },
+    {
+      today: new Date(),
+    }
+  );
+
+  useInterval(() => {
+    dispatch({ type: "setToday", today: new Date() });
+  });
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">UseReducerClock</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Div className="flex flex-col items-center justify-center h-24">
+      <Title className="text-5xl">UseReducer Clock</Title>
+      <Title className="mt-4 text-3xl">{today.toLocaleTimeString()}</Title>
+      <Subtitle className="mt-4 text-2xl">
+        {today.toLocaleDateString()}
+      </Subtitle>
+    </Div>
   );
 };
+
