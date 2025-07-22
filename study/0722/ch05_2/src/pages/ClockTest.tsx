8e5bd609018c294c4ee6ad5860455610bcd4ba45
narahub123 <parknara0213@gmail.com>
2025-07-22 15:42:43 +0900

    0722 시계 만들기

--- study/0722/ch05_2/src/pages/ClockTest.tsx
+++ study/0722/ch05_2/src/pages/ClockTest.tsx
@@ -1,8 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Title } from "../components";
+import * as C from "../store/clock";
+import { AppState } from "../store";
+import { useInterval } from "../hooks";
+
 export const ClockTest = () => {
+  const clock = new Date(
+    useSelector<AppState, C.State>((state) => state.clock)
+  );
+
+  const dispatch = useDispatch();
+
+  useInterval(() => dispatch(C.setClock(new Date().toISOString())));
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">ClockTest</h2>
-      <div className="mt-4"></div>
+      <Title>ClockTest</Title>
+
+      <div className="flex flex-col items-center mt-4">
+        <p className="text-2xl text-blue-600 text-bold">
+          {clock.toLocaleTimeString()}
+        </p>
+        <p className="text-lg text-blue-600 text-bold">
+          {clock.toLocaleDateString()}
+        </p>
+      </div>
     </section>
   );
 };
