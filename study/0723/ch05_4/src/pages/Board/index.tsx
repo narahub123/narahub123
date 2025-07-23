5cb5cf57cdfb44250c96b86b4fead618536d5724
john <test@google.com>
2025-07-23 23:05:01 +0900

    0723 트렐로 Board에 BoardList 적용

--- study/0723/ch05_4/src/pages/Board/index.tsx
+++ study/0723/ch05_4/src/pages/Board/index.tsx
@@ -1,14 +1,34 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { Title } from "../../components";
 import { CreateListForm } from "./CreateListForm";
-import { useListEntitiesStore } from "../../store/useListEntitiesStore";
-import { useListidOrdersStore } from "../../store";
+import { useListEntitiesStore, useListidOrdersStore } from "../../store";
+import BoardList from "../BoardList";
 
 export const Board = () => {
+  const listidOrders = useListidOrdersStore((state) => state.listidOrders);
+
+  // 먼저 listidOrders를 기반으로 uuid 배열을 memo
+  const uuids = useMemo(() => listidOrders, [listidOrders]);
+
+  // 이 상태에서 store 내부 접근은 한 번만 하도록 구조 변경
+  const listEntities = useListEntitiesStore((state) => state.listEntities);
+
+  const lists = useMemo(
+    () => uuids.map((uuid) => listEntities[uuid]),
+    [uuids, listEntities]
+  );
+
   const addListidToOrders = useListidOrdersStore(
     (state) => state.addListidToOrders
   );
+
+  const removeListidFromOrders = useListidOrdersStore(
+    (state) => state.removeListidFromOrders
+  );
+
   const addList = useListEntitiesStore((state) => state.addList);
+
+  const removeList = useListEntitiesStore((state) => state.removeList);
 
   const onCreateList = useCallback((uuid: string, title: string) => {
     const list = { uuid, title };
@@ -17,10 +37,30 @@ export const Board = () => {
     addList(list);
   }, []);
 
+  const onRemoveList = useCallback((listid: string) => {
+    removeList(listid);
+    removeListidFromOrders(listid);
+  }, []);
+
+  const children = useMemo(
+    () =>
+      lists.map((list) => (
+        <BoardList
+          key={list.uuid}
+          list={list}
+          onRemoveList={() => {
+            onRemoveList(list.uuid);
+          }}
+        />
+      )),
+    [lists, onRemoveList]
+  );
+
   return (
     <section className="mt-4">
       <Title>Board</Title>
-      <div className="mt-4">
+      <div className="flex flex-wrap p-2 mt-4">
+        {children}
         <CreateListForm onCreateList={onCreateList} />
       </div>
     </section>
