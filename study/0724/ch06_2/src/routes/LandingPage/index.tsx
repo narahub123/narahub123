2064a22338c6f35515b689ef1a2cd86435972512
narahub123 <parknara0213@gmail.com>
2025-07-24 15:57:31 +0900

    0724 ch06_2 색인 라우트

--- study/0724/ch06_2/src/routes/LandingPage/index.tsx
+++ study/0724/ch06_2/src/routes/LandingPage/index.tsx
@@ -1,10 +1,10 @@
-const CopyMe = () => {
+const LandingPage = () => {
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
+      <h2 className="text-5xl font-bold text-center">LandingPage</h2>
       <div className="mt-4"></div>
     </section>
   );
 };
 
-export default CopyMe;
+export default LandingPage;
