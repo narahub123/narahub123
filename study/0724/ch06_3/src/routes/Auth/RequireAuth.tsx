295d4ff25fe25d1cc131b7e6139400a9d702e64f
narahub123 <parknara0213@gmail.com>
2025-07-24 21:59:49 +0900

    0724 ch06_3 RequireAuth 추가

--- study/0724/ch06_3/src/routes/Auth/RequireAuth.tsx
+++ study/0724/ch06_3/src/routes/Auth/RequireAuth.tsx
@@ -1,8 +1,18 @@
-export const RequireAuth = () => {
-  return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">RequireAuth</h2>
-      <div className="mt-4"></div>
-    </section>
-  );
+import { FC, PropsWithChildren, useEffect } from "react";
+import { useAuth } from "../../contexts";
+import { useNavigate } from "react-router-dom";
+
+type RequireAuthProps = {};
+
+export const RequireAuth: FC<PropsWithChildren<RequireAuthProps>> = ({
+  children,
+}) => {
+  const navigate = useNavigate();
+  const { loggedUser } = useAuth();
+
+  useEffect(() => {
+    if (!loggedUser) navigate(-1);
+  }, [loggedUser, navigate]);
+
+  return <>{children}</>; // 허가된 사용자: chilren이 element가 되도록 함
 };
