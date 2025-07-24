ee7d5b749126c6204796baddf762bb35301692bd
john <test@google.com>
2025-07-24 14:40:57 +0900

    0724 Link 사용법

--- study/0724/ch06_1/src/routes/Home.tsx
+++ study/0724/ch06_1/src/routes/Home.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 type HomeProps = {
   title?: string;
 };
 
 const Home: FC<HomeProps> = ({ title }) => {
-  return <p>{title ?? "Home"}</p>;
+  return (
+    <div>
+      <div className="flex p-4 space-x-4 bg-gray-200">
+        <Link to="/">Home</Link>
+        <Link to="/welcome">Welcome</Link>
+        <Link to="/board">Board</Link>
+      </div>
+      <p className="text-xl text-center text-bold">{title ?? "Home"}</p>;
+    </div>
+  );
 };
 
 export default Home;
