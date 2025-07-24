eb25894e338eaac8f60ac33b017d3970f4e27e1d
john <test@google.com>
2025-07-24 21:52:24 +0900

    0724 ch06_3 Logout 컴포넌트 추가

--- study/0724/ch06_3/src/routes/Auth/Logout.tsx
+++ study/0724/ch06_3/src/routes/Auth/Logout.tsx
@@ -1,8 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { useToggle } from "../../hooks";
+import { useAuth } from "../../contexts";
+import { useCallback } from "react";
+import { Modal, ModalAction, ModalContent } from "../../theme/daisyui";
+
 export const Logout = () => {
+  const navigate = useNavigate();
+  const [open, toggleOpen] = useToggle(true);
+
+  const { logout } = useAuth();
+
+  const onAccept = useCallback(() => {
+    logout(() => {
+      toggleOpen();
+      navigate("/");
+    });
+  }, [navigate, toggleOpen]);
+
+  const onCancel = useCallback(() => {
+    toggleOpen();
+    navigate(-1);
+  }, [navigate, toggleOpen]);
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">Logout</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Modal open={open}>
+      <ModalContent
+        closeIconClassName="btn-primary btn-online"
+        onCloseIconClicked={onCancel}
+      >
+        <p className="text-xl text-center">Are you sure you want to log out?</p>
+        <ModalAction>
+          <button className="btn btn-primary btn-sm" onClick={onAccept}>
+            Logout
+          </button>
+          <button className="btn btn-secondary btn-sm" onClick={onCancel}>
+            Cancel
+          </button>
+        </ModalAction>
+      </ModalContent>
+    </Modal>
   );
 };
