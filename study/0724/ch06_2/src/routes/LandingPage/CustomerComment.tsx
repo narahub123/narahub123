e8388d31d6a91f02b3a0b025d9474f65231d7c18
john <test@google.com>
2025-07-24 18:04:05 +0900

    0724 ch06_2 프로모션 영역 추가

--- study/0724/ch06_2/src/routes/LandingPage/CustomerComment.tsx
+++ study/0724/ch06_2/src/routes/LandingPage/CustomerComment.tsx
@@ -1,8 +1,34 @@
-export const CustomerComment = () => {
+import { FC } from "react";
+import { CustomerComment as CustomerCommentType } from "../../data";
+import { Avatar, Div } from "../../components";
+
+export type CustomerCommentProps = {
+  customerComment: CustomerCommentType;
+};
+
+export const CustomerComment: FC<CustomerCommentProps> = ({
+  customerComment,
+}) => {
+  const { name, jobTitle, company, avatar, comment } = customerComment;
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CustomerComment</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Div
+      className="relative p-2 mx-2 mt-8 border-2 rounded-lg shadow-lg board-primary"
+      minWidth="20rem"
+      width="20rem"
+      minHeight="10rem"
+    >
+      <div className="absolute flex items-center justify-center w-full -top-7">
+        <Avatar src={avatar} className="border-2 border-primary" />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col p-4 font-bold">
+          <p>{name}</p>
+          <p>{jobTitle}</p>
+          <p>{company}</p>
+        </div>
+        <p className="mt-4">{comment}</p>
+      </div>
+    </Div>
   );
 };
