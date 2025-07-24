81f3ebb85f366545cafb0b19deee9b4061007a21
john <test@google.com>
2025-07-24 15:39:37 +0900

    0724 ch06_2 설정

---
+++ study/0724/ch06_2/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd/dist/core";
+import { BrowserRouter } from "react-router-dom";
+import { RoutesSetup } from "./routes";
+
+function App() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <BrowserRouter>
+        <RoutesSetup />
+      </BrowserRouter>
+    </DndProvider>
+  );
+}
+
+export default App;
