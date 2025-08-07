73033110986ed5ea12256e833740f068ca9af154
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-08-07 09:09:14 +0900

    [level 1] Title: 2016년, Time: 7.40 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,11 +1,9 @@
 function solution(a, b) {
-    const days = ['SUN','MON','TUE','WED','THU','FRI','SAT']
-    
     const date = new Date(2016, a -1, b);
     
-    const day  = days[date.getDay()];
     
     
-    var answer = day;
+    
+    var answer = date.toString().slice(0, 3).toUpperCase();
     return answer;
 }
