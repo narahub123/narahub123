bc80ca6cb04bcadc7fc5c35157f4fee8743493f0
narahub123 <parknara0213@gmail.com>
2025-08-09 23:54:35 +0900

    0809 ResetPasswordModal value, canSend, isValid 상태 추가

--- mini-projects/chatting-app/client/src/components/ResetPasswordModal.tsx
+++ mini-projects/chatting-app/client/src/components/ResetPasswordModal.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useOpenStore } from "../stores";
 import { Button, Modal, ModalContent } from "../theme/daisyui";
-import Input from "./Input";
+import { Input } from "../components";
+
+const initialValue = { email: "", password: "" };
 
 const ResetPasswordModal = () => {
+  const [value, setValue] = useState(initialValue);
+  const [canSend, setCanSend] = useState(false);
+  const [isValid, setIsValid] = useState(false);
   // 비밀번호 재설정 모달 여닫기 상태
   const isOpen = useOpenStore((state) => state.isResetPasswordModalOpen);
 
@@ -22,7 +28,49 @@ const ResetPasswordModal = () => {
     setIsResetPasswordModalOpen(false);
     // 로그인 모달 열기
     setIsLoginModalOpen(true);
+    // value 초기화
+    setValue(initialValue);
+    // isValid 초기화
+    setIsValid(false);
+    // canSend 초기화
+    setCanSend(false);
   };
+
+  // 입력값 함수
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setValue((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+
+    if (value) {
+      setCanSend((prev) => {
+        if (prev !== true) return true;
+        else return prev;
+      });
+    } else {
+      setCanSend((prev) => {
+        if (prev !== false) return false;
+        else return prev;
+      });
+    }
+  };
+
+  // 이메일 확인
+  const handleEmailCheck = () => {
+    setIsValid((prev) => (prev !== true ? true : prev));
+    setCanSend((prev) => (prev !== false ? false : prev));
+  };
+
+  // 비밀번호 확인
+  const handlePasswordCheck = () => {
+    setIsValid((prev) => (prev !== true ? true : prev));
+  };
+
+  console.log(value);
 
   return (
     <Modal open={isOpen}>
@@ -30,25 +78,63 @@ const ResetPasswordModal = () => {
         <div className="space-y-2">
           <h2 className="text-xl font-bold">비밀번호 재설정</h2>
         </div>
-        <div className="space-y-2">
-          <p>비밀번호를 재설정할 계정의 이메일을 작성해주세요.</p>
-          <Input
-            field="email"
-            placeholder="이메일"
-            type="email"
-            onChange={() => {}}
-            entity={{}}
-          />
-        </div>
-        <div className="flex justify-between">
-          <Button
-            className="text-white btn btn-error"
-            onClick={handleResetPasswordCancel}
-          >
-            취소
-          </Button>
-          <Button className="btn btn-primary">확인</Button>
-        </div>
+        {isValid ? (
+          <>
+            <div className="space-y-2">
+              <p>이메일로 전송된 임시 비밀번호를 입력해주세요</p>
+              <Input
+                field="password"
+                placeholder="비밀번호"
+                type="password"
+                onChange={handleChange}
+                entity={value}
+              />
+            </div>
+            <div className="flex justify-between">
+              <Button
+                className="text-white btn btn-error"
+                onClick={handleResetPasswordCancel}
+              >
+                취소
+              </Button>
+              <Button
+                className="btn btn-primary"
+                disabled={!canSend}
+                onClick={canSend ? handlePasswordCheck : undefined}
+              >
+                확인
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="space-y-2">
+              <p>비밀번호를 재설정할 계정의 이메일을 작성해주세요.</p>
+              <Input
+                field="email"
+                placeholder="이메일"
+                type="email"
+                onChange={handleChange}
+                entity={value}
+              />
+            </div>
+            <div className="flex justify-between">
+              <Button
+                className="text-white btn btn-error"
+                onClick={handleResetPasswordCancel}
+              >
+                취소
+              </Button>
+              <Button
+                className="btn btn-primary"
+                disabled={!canSend}
+                onClick={canSend ? handleEmailCheck : undefined}
+              >
+                확인
+              </Button>
+            </div>
+          </>
+        )}
       </ModalContent>
     </Modal>
   );
