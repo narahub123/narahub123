66623983a3be69ddb9e51f671c75a3d949278f22
narahub123 <parknara0213@gmail.com>
2025-08-10 12:57:54 +0900

    0810 controller 설정

---
+++ mini-projects/chatting-app/server/src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from "./authController";
+export * from "./chatController";
+export * from "./chatroomController";
+export * from "./userController";
+export * from "./notificationController";
