1195351bd373cf641804261c9ccd8417dbeff418
narahub123 <parknara0213@gmail.com>
2025-08-13 01:06:49 +0900

    0813 WidgetButton 클릭 함수 추가

--- mini-projects/chatting-app/client/src/components/WidgetButton.tsx
+++ mini-projects/chatting-app/client/src/components/WidgetButton.tsx
@@ -5,21 +5,26 @@ import { useLoginStore, useOpenStore } from "../stores";
 const WidgetButton: FC = () => {
   const isLoggedIn = useLoginStore((state) => state.isLoggedIn);
   const setIsAuthMenuOpen = useOpenStore((state) => state.setIsAuthMenuOpen);
+  const setIsChatModalOpen = useOpenStore((state) => state.setIsChatModalOpen);
 
   const handleAuthMenu = () => {
     setIsAuthMenuOpen((prev: boolean) => !prev);
   };
 
+  const handleChatModalOpen = () => {
+    setIsChatModalOpen(true);
+  };
+
   return (
     <button className="fixed bottom-5 right-5">
       <div
-        className="w-16 h-16 rounded-full bg-blue-400 flex justify-center items-center shadow-lg hover:bg-blue-500"
-        onClick={isLoggedIn ? () => {} : () => handleAuthMenu()}
+        className="flex items-center justify-center w-16 h-16 bg-blue-400 rounded-full shadow-lg hover:bg-blue-500"
+        onClick={isLoggedIn ? handleChatModalOpen : () => handleAuthMenu()}
       >
         {isLoggedIn ? (
-          <Icon name="chat_bubble" className="font-bold text-4xl text-white" />
+          <Icon name="chat_bubble" className="text-4xl font-bold text-white" />
         ) : (
-          <Icon name="fingerprint" className="font-bold text-5xl text-white" />
+          <Icon name="fingerprint" className="text-5xl font-bold text-white" />
         )}
       </div>
     </button>
