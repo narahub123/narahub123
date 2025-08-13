865c226846835cf3e96ca4bd9684c2f986ee8c17
narahub123 <parknara0213@gmail.com>
2025-08-13 11:24:04 +0900

    0813 google strategy 만들기

--- study/0813/chapter11/nest-auth-test/src/auth/auth.module.ts
+++ study/0813/chapter11/nest-auth-test/src/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { SessionSerializer } from './session.serializer';
 import { LocalStrategy } from './local.strategy';
+import { GoogleStrategy } from './google.strategy';
 
 @Module({
   imports: [UserModule, PassportModule.register({ session: true })], // 패스포트 모둘 추가
-  providers: [AuthService, LocalStrategy, SessionSerializer], // 프로바이더 설정 추가
+  providers: [AuthService, LocalStrategy, SessionSerializer, GoogleStrategy], // 프로바이더 설정 추가
   controllers: [AuthController],
 })
 export class AuthModule {}
