075148788c15b896790ae7bdf6fcab80e666bdc5
narahub123 <parknara0213@gmail.com>
2025-08-13 22:11:38 +0900

    0813 ChatSidebar 수정

--- mini-projects/chatting-app/client/src/components/ChatSidebar.tsx
+++ mini-projects/chatting-app/client/src/components/ChatSidebar.tsx
@@ -6,7 +6,10 @@ const ChatSidebar = () => {
     <aside className="flex flex-col justify-between p-2 border-r h-60">
       <div className="flex flex-col">
         {sidebars.map((sidebar) => (
-          <Button className="btn rounded-full w-[50px] aspect-square bg-transparent border-0 shadow-none p-3">
+          <Button
+            className="btn rounded-full w-[50px] aspect-square bg-transparent border-0 shadow-none p-3"
+            key={sidebar.src}
+          >
             <img
               src={sidebar.src}
               alt={sidebar.alt}
@@ -16,12 +19,10 @@ const ChatSidebar = () => {
         ))}
       </div>
       <div>
-        <Button className="btn rounded-full w-[50px] aspect-square bg-transparent border-0 shadow-none">
-          <Icon
-            name="more_horiz"
-            className="text-2xl bg-transparent border-0 shadow-none"
-          />
-        </Button>
+        <Icon
+          name="more_horiz"
+          className="text-2xl bg-transparent border-0 shadow-none"
+        />
       </div>
     </aside>
   );
