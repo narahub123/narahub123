a3e1f2b66b92e3d478b3e8a5efcaff2ecd64e4e6
narahub123 <parknara0213@gmail.com>
2025-08-13 15:11:15 +0900

    0813 파일 업로드 api 테스트

---
+++ study/0813/chapter12/nest-file-upload/src/app.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('file-upload')
+  @UseInterceptors(FileInterceptor('file'))
+  fileUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString('utf-8'));
+
+    return 'File Upload';
+  }
+}
