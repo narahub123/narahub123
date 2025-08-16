e6852e9b3e85c39987a36437f53023ceaf9e3110
narahub123 <parknara0213@gmail.com>
2025-08-16 23:18:09 +0900

    0816 웹소켓을 이용한 방 생성

--- mini-projects/chatting-app/server/src/websocket/index.ts
+++ mini-projects/chatting-app/server/src/websocket/index.ts
@@ -1,26 +1,55 @@
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import http from "http";
 
 export default (
   server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
 ) => {
-  const webSocket = new WebSocket.Server({ server });
+  const rooms: Record<
+    string,
+    WebSocket.Server<typeof WebSocket, typeof http.IncomingMessage>
+  > = {};
 
-  webSocket.on("connection", (ws) => {
-    ws.send("connected");
+  server.on("upgrade", (req, socket, head) => {
+    const { pathname, searchParams } = new URL(req.url!, "ws://localhost:3301");
 
-    ws.on("message", (msg) => {
-      const message = JSON.parse(msg.toString());
+    const roomId = searchParams.get("roomId");
 
-      webSocket.clients.forEach((client) => {
-        client.send(JSON.stringify(message));
+    if (!roomId) return;
+    console.log(pathname, roomId);
+
+    if (rooms[roomId]) {
+      rooms[roomId].handleUpgrade(req, socket, head, (ws) => {
+        rooms[roomId].emit("connection", ws, req);
+      });
+    } else {
+      const newRoom = new WebSocketServer({ noServer: true });
+
+      newRoom.on("connection", (ws) => {
+        ws.on("error", console.error);
+
+        console.log(`${roomId}방에 연결됨`);
+        ws.send("connected");
+
+        ws.on("message", (msg) => {
+          const message = JSON.parse(msg.toString());
+
+          newRoom.clients.forEach((client) => {
+            client.send(JSON.stringify(message));
+          });
+
+          console.log("메시지 전송");
+        });
+
+        ws.on("close", () => {
+          console.log("클라언트와 접속 해제");
+        });
       });
 
-      console.log("메시지 전송");
-    });
+      rooms[roomId] = newRoom;
 
-    ws.on("close", () => {
-      console.log("클라언트와 접속 해제");
-    });
+      newRoom.handleUpgrade(req, socket, head, (ws) => {
+        rooms[roomId].emit("connection", ws, req);
+      });
+    }
   });
 };
