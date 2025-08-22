e537e3ccc1dadc53270706711738926ca7eb38d6
narahub123 <parknara0213@gmail.com>
2025-08-22 23:05:42 +0900

    [yunspeech] backend index.ts

---
+++ mini-projects/yunspeech/backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3302;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log("서버가 3302에 연결됨"));
