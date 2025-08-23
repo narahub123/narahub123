526ca54a1a94d3a72503a9bd1e00b713f91f466c
narahub123 <parknara0213@gmail.com>
2025-08-23 19:59:15 +0900

    [yunspeech] 배포

--- mini-projects/yunspeech/frontend/src/router/index.tsx
+++ mini-projects/yunspeech/frontend/src/router/index.tsx
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AdminPage, HomePage } from "../pages";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/admin",
+      element: <AdminPage />,
+    },
+  ],
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/admin",
-    element: <AdminPage />,
-  },
-]);
+    basename: "/narahub123/yunspeech",
+  }
+);
