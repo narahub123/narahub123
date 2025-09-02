d325f23221cfc341184530f3dd144a48aa46f247
Naraaaa <93567002+narahub123@users.noreply.github.com>
2025-09-02 09:41:19 +0900

    [level 1] Title: 소수 만들기, Time: 30.50 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,38 +1,25 @@
 function solution(nums) {
-    // nums의 3개의 숫자 조합 
-    let primes = [];
-    
-    for(let i = 0; i < nums.length; i++){
-        
-        for (let j = i + 1; j < nums.length; j++){   
-            
-            for(let k = j + 1; k < nums.length ; k++){
-                
-                const num = nums[i] + nums[j] + nums[k];
-                
-                // 소수 판별
-                if(isPrime(num)){
-                    primes.push(num);
-                }
-            }
-        }
-        
-    }
-    
-    return primes.length;
-}
+  // 가능한 최대 합 (가장 큰 3개 합)
+  const top3sum = [...nums].sort((a, b) => b - a).slice(0, 3)
+    .reduce((acc, v) => acc + v, 0);
 
-function isPrime(num){
-    if(num === 2){
-        return true;
+  // 소수 테이블 생성 (에라토스테네스 체)
+  const prime = Array(top3sum + 1).fill(true);
+  prime[0] = prime[1] = false;
+  for (let p = 2; p * p <= top3sum; p++) {
+    if (prime[p]) {
+      for (let q = p * p; q <= top3sum; q += p) prime[q] = false;
     }
-    
-    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
-        if(num % i === 0 ){
-            return false;
-        }
-    }
-    
-    return true;
-}
+  }
 
+  let count = 0;
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const s = nums[i] + nums[j] + nums[k];
+        if (prime[s]) count++;
+      }
+    }
+  }
+  return count;
+}
